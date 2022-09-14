6e0e7d6d1e2ef3cf3ba1fd1f6a4ccc94ebe1c211
Damian McDonald <damiamcd@amazon.es>
2022-09-14 12:12:35 +0300

    Adds Jinja2 auto escaping

--- stacks/weighted_alb_with_waf/resources/lambda/orchestrator/app/services/notifier_service.py
+++ stacks/weighted_alb_with_waf/resources/lambda/orchestrator/app/services/notifier_service.py
@@ -30,7 +30,13 @@ class NotifierService:
 
     TEMPLATE_DIR = dirname(dirname(abspath(__file__)))
     TEMPLATE_LOADER = jinja2.FileSystemLoader(searchpath=f"{TEMPLATE_DIR}/templates")
-    TEMPLATE_ENV = jinja2.Environment(loader=TEMPLATE_LOADER)
+    TEMPLATE_ENV = jinja2.Environment(
+        loader=TEMPLATE_LOADER,
+        autoescape=jinja2.select_autoescape(
+            default_for_string=True,
+            default=True
+        )
+    )
 
 
     def get_notification_template(
